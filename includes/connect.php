8154da48d01d9402bc6d175e14cf1a7d9dc64529
maximeoger <maximeoger93@gmail.com>
2018-05-01 18:55:48 +0200

    add administration interface

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,8 @@
 
 
 try {
-    $kandt_db = new PDO('mysql:host=localhost;dbname=kandt;host=127.0.0.1','root','root');
+    $pdo = new PDO('mysql:host=localhost;dbname=kandt;host=127.0.0.1','root','root');
 } catch(PDOException $e) {
     die("Erreur ! :".$e->getMessage());
 }
+$pdo->exec('SET NAMES UTF8');
--- includes/functions.php
+++ includes/functions.php
@@ -1,18 +1,132 @@
 <?php
-require_once'includes/connect.php';
-
-// generation automatique des LIs en fonction
-// du nom de la page appellée via 'title' dans $data
-function createLi($linkText, $page, $activePage)
+/**
+ * generates nav LI element
+ *
+ * @param string $slug page name with .php at the end
+ * @param string $linkText text that goes in the link (d'uhhhh)
+ */
+function li($slug, $linkText, $pageKey)
 {
     $active = "";
-    // si le contenu de la variable $page est strictement égal
-    // a la dernière composante de l'url (donc le nom de la page)
-    if ($page === $activePage) {
+    if($slug === $pageKey){
         $active = ' class="active" ';
     }
-    // Select slug from
     ?>
-    <li<?= $active ?>><a href="?page=<?= $page ?>"><?= $linkText ?></a></li>
+    <li<?=$active?>><a href="<?php printf("%s%s=%s", APP_URL_BASE, APP_PAGE_PARAM, $slug)?>"><?=$linkText?></a></li>
     <?php
+}
+
+/**
+ * display one page
+ *
+ * @param array $page
+ */
+function displayPage(array $page) : void
+{
+
+    if(isset($_GET['page']) && $_GET['page'] === 'admin'){
+        ?>
+        <div class="container theme-showcase" role="main">
+            <div class="jumbotron">
+                <h1>Admin</h1>
+                <p>Interface d'administration des pages</p>
+            </div>
+        </div>
+        <?php
+
+    }else{
+        ?>
+        <div class="container theme-showcase" role="main">
+            <div class="jumbotron">
+                <h1><?=$page['h1']?></h1>
+                <p><?=$page['p']?></p>
+                <span class="label <?=$page['span-class']?>"><?=$page['span-text']?></span>
+            </div>
+            <img class="img-thumbnail" alt="<?=$page['img-alt']?>" src="img/<?=$page['img-src']?>" data-holder-rendered="true">
+        </div>
+        <?php
+    }
+}
+
+
+/**
+ * @param PDO $pdo database connection
+ * @param string $slug page slug
+
+ * @throws Exception SQL query Fed up
+
+ * @return array|null
+ */
+function getPageContent(\PDO $pdo, string $slug): ?array
+{
+        $sql = "SELECT 
+            `h1`, 
+            `p`, 
+            `span-class`, 
+            `span-text`, 
+            `img-alt`, 
+            `img-src` 
+        FROM 
+          `pages` 
+        WHERE 
+          `slug` = :slug
+        LIMIT 1
+        ;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":slug", $slug, PDO::PARAM_STR);
+        $stmt->execute();
+        handlePDOError($stmt);
+        if (false === $row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+            return null;
+        }
+        return $row;
+}
+/**
+ * description
+ *
+ * @param PDO $pdo
+ *
+ * @throws Exception
+ *
+ * @return array
+ */
+function getNavData(\PDO $pdo): array
+{
+    $sql = "SELECT 
+              `slug`, 
+              `nav-title` 
+            FROM 
+              `pages`
+        ;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    handlePDOError($stmt);
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+/**
+ * @param PDOStatement $stmt
+ * @throws Exception
+ */
+function handlePDOError(PDOStatement $stmt): void
+{
+    if ($stmt->errorCode() != '00000') {
+        throw new \Exception($stmt->errorInfo()[1]);
+    }
+}
+/**
+ * display navigation links
+ * @param array $navData
+ * @param string $pageKey
+ */
+function displayNav(array $navData, string $pageKey): void
+{
+    if (count($navData) === 0) {
+        die("No pages, d'uh");
+    }
+    foreach ($navData as $onePage) {
+        li($onePage['slug'], $onePage['nav-title'], $pageKey);
+    }
 }
